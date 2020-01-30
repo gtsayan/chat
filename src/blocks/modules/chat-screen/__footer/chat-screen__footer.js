dea0035efd9ee13b7e023b3a3512ce6c1da8530e
gtsayan <48355689+gtsayan@users.noreply.github.com>
2020-01-30 18:28:22 +0300

    final

---
+++ src/blocks/modules/chat-screen/__footer/chat-screen__footer.js
@@ -0,0 +1,13 @@
+modules.define('chat-screen__footer', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declElem('chat-screen', 'footer', {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
--- src/blocks/modules/contacts-list/contacts-list.js
+++ src/blocks/modules/contacts-list/contacts-list.js
@@ -1,4 +1,4 @@
-modules.define('chat-screen-footer', ['i-bem-dom'], function(provide, bemDom) {
+modules.define('contacts-list', ['i-bem-dom'], function(provide, bemDom) {
 
 provide(bemDom.declBlock(this.name, {
     onSetMod: {
